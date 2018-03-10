068c749410595608f7b06c17cc9b22108ef75e88
pr-1 <prince17.ps6@gmail.com>
2018-03-10 14:33:06 +0530

    server issue

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ app.get('/', (req, res) => {
 });
 
 // Start Server
-app.listen(process.env.port || 3000, () => {
-  console.log('Server started on * 3000');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
